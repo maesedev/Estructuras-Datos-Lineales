1f02507a03725b5e5b776ab10a49e73bee4e9235
santiago Garzon <79063950+maesedev@users.noreply.github.com>
2023-08-01 16:21:40 +0000

    Ejercicios del 01/08/2023

--- laboratio.py
+++
@@ -1,9 +0,0 @@
-
-def sum_array(array,i):
-    if i == 0:
-        return array[0]
-    return int(array[i]) + int(sum_array(array , i - 1))
-
-Array = [5,10,25,65,32,45]
-
-print(sum_array(Array , 5 ))
