d223e65bff2afc199223c9f777e17325219fed3f
santiago Garzon <79063950+maesedev@users.noreply.github.com>
2023-08-07 22:46:53 +0000

    Punto 4

--- taller.js
+++ taller.js
@@ -1,4 +1,5 @@
-
+let A = [1,2,3,4,5,6,7]
+let B = [7,6,5,4,3,2,1]
 
 /*Ejercicio 1
 Cree un programa que implemente funciones recursivas y funciones iterativas que reciban
@@ -15,19 +16,68 @@ Cree un programa que implemente funciones recursivas y funciones iterativas que 
 function SumArrays(arrA,arrB){
     let arrC = []
     function sumByIndex(i){
+        arrC.unshift(arrA[i] + arrB[i])
 
-        if(i === 0){
-            arrC[0] = arrA[0] + arrB[0]
-            return arrC
-        }else {
-            arrC[i] = arrA[i] + arrB[i]
-            sumByIndex(i - 1)
-        } 
+        if(i === 0) return arrC
+
+        sumByIndex(i - 1)
     }
+
     sumByIndex(arrA.length - 1)
+
     return arrC
 }
-let A = [1,2,15,4,5,6,7]
-let B = [1,2,33,-20,5,6,7]
+let C = SumArrays(A,B)
+console.log("\n\n----------Ejercicio 1-a----------")
+console.log( C );
 
-console.log(SumArrays(A, B , A.length - 1));
+//1-b
+
+function isEven(n){
+    return n % 2 === 1
+}
+
+function SumArraysElementosImpares(A, i = A.length-1){
+    if(i === 0) return 0
+    if(isEven(A[i])) return A[i] + SumArraysElementosImpares(A,i-1)
+    else return SumArraysElementosImpares(A,i-1)
+}
+
+
+console.log("\n\n----------Ejercicio 1-b----------")
+console.log(A);
+console.log(SumArraysElementosImpares(A));
+
+
+/*
+Ejercicio 3
+    Cree un programa que implemente una función recursiva y función iterativa que reciba como
+    parámetros dos arreglos 𝑎𝑟𝑟𝑎𝑦1 𝑎𝑟𝑟𝑎𝑦2 no vacíos de números enteros positivos o negativos y
+    retorne un nuevo arreglo 𝑎𝑟𝑟𝑎𝑦3 con los elementos de ambos arreglos.
+    Ejemplo:
+        Entrada f([2, 10, 99], [-1, 8, 200])
+        Salida [-1,2,8.10,99,200]
+*/
+
+
+
+function juntarElementos(arrA,arrB){
+    let i = arrA.length - 1
+    let arrC = []
+    const sumByIndex = (i)=>{
+
+
+        arrC.unshift(...order( arrA[i] , arrB[i] ) );
+        if(i === 0) return arrC
+        return sumByIndex(i - 1)
+    }
+    const order = (a,b) => a < b ? [a,b] :[b,a]  
+
+    return sumByIndex(i)
+}
+
+console.log("\n\n----------Ejercicio 4 ----------")
+
+console.log(juntarElementos(
+    [2, 10, 99],
+    [-1, 8, 200]));
