1fa2deeb36624288b731fdb7971251b0045107dd
santiago Garzon <79063950+maesedev@users.noreply.github.com>
2023-08-07 19:47:39 +0000

    Punto 1

--- taller.js
+++ taller.js
@@ -1,33 +1,33 @@
-array1 = [1,2,3,4,5,6,7]
-array2 = [1,2,3,4,5,6,7,8]
-array3 = [1,2,3,100,5,6,7]
 
 
-function equals(arr1,arr2, i = arr1.length - 1){
-    if(arr1.length !== arr2.length)return false
-    if(i == 0) return true
-    if(arr1[i] === arr2[i]) return  true && equals(arr1, arr2, i -1)
-    else return false
+/*Ejercicio 1
+Cree un programa que implemente funciones recursivas y funciones iterativas que reciban
+    como parámetro dos arreglos 𝑎𝑟𝑟𝑎𝑦1 𝑦 𝑎𝑟𝑟𝑎𝑦2 no vacíos de números enteros naturales y
+    realicen las siguientes operaciones:
+    a. Función que retorne un nuevo arreglo 𝑎𝑟𝑟𝑎𝑦3 con la suma de los elementos de ambos
+        arreglos 𝑎𝑟𝑟𝑎𝑦1[𝑖] + 𝑎𝑟𝑟𝑎𝑦2[𝑖]
 
+        b. Función para sumar los datos del arreglo 𝑎𝑟𝑟𝑎𝑦1, la función debe retorna el resultado
+        de la suma de los datos impares.
+        */
+
+// 1-a
+function SumArrays(arrA,arrB){
+    let arrC = []
+    function sumByIndex(i){
+
+        if(i === 0){
+            arrC[0] = arrA[0] + arrB[0]
+            return arrC
+        }else {
+            arrC[i] = arrA[i] + arrB[i]
+            sumByIndex(i - 1)
+        } 
+    }
+    sumByIndex(arrA.length - 1)
+    return arrC
 }
+let A = [1,2,15,4,5,6,7]
+let B = [1,2,33,-20,5,6,7]
 
-console.log(equals(array1, array2));
-console.log(equals(array1, array3));
-
-
-function sumarArr(){
-}
-
-console.log(equals(array1, array2));
-console.log(equals(array1, array3));
-
-
-
-function findIndex(Arr, n, i = Arr.length){
-    if( i = -1) return -1
-    if( Arr[i] == n ) return i
-    else return findIndex(Arr, n, i - 1)
-}
-
-console.log(findIndex(array1,5))
-
+console.log(SumArrays(A, B , A.length - 1));
