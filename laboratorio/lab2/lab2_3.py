e6046e3929e40e92121f79ad5658e21b58903c5d
santiago Garzon <79063950+maesedev@users.noreply.github.com>
2023-08-24 13:58:12 +0000

    agregamos funcion de eliminar

--- laboratorio/lab2/lab2_3.py
+++ laboratorio/lab2/lab2_3.py
@@ -4,19 +4,19 @@ from random import randint
 
 def app():
     """Función principal"""
-    Productos = crearProductos()
-    MostrarProductos(Productos)
-    ConCodigoPar(Productos)
+    Productos = crear_productos()
+    mostrar_productos(Productos)
+    con_codigo_par(Productos)
     promedio(Productos)
 
-def crearProductos():
+def crear_productos():
     """Crear una la lista encadenada que contenga instancias de la clase Producto, cada producto al
     crearse tiene los siguientes datos: código, nombre, cantidad y precio (los datos cantidad y
     precio se generan aleatoriamente), el código del producto será un numero consecutivo que
     debe iniciar en 1000, el nombre del producto será básico (producto1, producto 2, producto3,
     productoN). El número de productos en la lista ser un número aleatorio entre 10 y 30"""
     lista = ListaEnlazada()
-    Id_producto = 1
+    id_producto = 1
     for i in range( randint(10,30) ):
         price = randint(1000,500000)
         cantidad = randint(1,1000)
@@ -24,17 +24,18 @@ def crearProductos():
         producto = Producto(
             codigo=1000 + i,
             cantidad=cantidad,
-            nombre=f"producto {Id_producto}",
+            nombre=f"producto {id_producto}",
             precio=price
             )
-        Id_producto += 1
+        id_producto += 1
         
         lista.add_at_end(producto)
 
     return lista
 
     
-def MostrarProductos(lista):
+def mostrar_productos(lista):
+    """Muestra todos los productos, y un null al final"""
     for i in range(lista.size()):
         product = lista.get(i)
         print(f"Codigo: {product.codigo}")
@@ -43,11 +44,9 @@ def MostrarProductos(lista):
         print(f"Cantidad: {product.cantidad}")
         print("\t|")
         print("\t↓")
-    
     print("\tNull")
-    
 
-def ConCodigoPar(lista):
+def con_codigo_par(lista):
     """Calcular el precio total de los productos cuyo código sea un número par y mostrar los
 datos de cada producto.
 """
@@ -66,21 +65,18 @@ datos de cada producto.
 def promedio(lista):
     """Mostrar por consola el precio medio de todos los productos y mostrar la cantidad de
 productos en la lista. Implementar una función (método) recursivo."""
-
     def recursive(suma,cantidad, i):
         """"""
         product = lista.get(i)
         if product is None:
             return [suma, cantidad]
-        return recursive(suma + product.precio, cantidad + product.cantidad,  i+1)
-    
+        return recursive(suma + product.precio, cantidad + product.cantidad,  i + 1)
     i = 0
-    
     [suma_precios,cant_productos] = recursive(0,0,i)
 
     print(f"\nLa cantidad total de productos es: {cant_productos}")
-    promedio = round(suma_precios / lista.size()) 
-    print(f"El precio medio de todos los  productos es: ${promedio}")
+    prom = round(suma_precios / lista.size())
+    print(f"El precio medio de todos los  productos es: ${prom}")
 
 
 
--- laboratorio/lab2/modules/linked_list.py
+++ laboratorio/lab2/modules/linked_list.py
@@ -18,11 +18,6 @@ class ListaEnlazada:
         self.head = Node(data=data, next=self.head)  
         self.numero_elementos += 1
 
-    def is_empty(self):
-        """Método para verificar si la estructura de datos esta vacia"""
-        return self.head is None
-
-    
     def add_at_end(self, data):
         """Método para agregar elementos al final de la linked list"""
         # Si no hay elementos entonces el ultimo es el primero, ya que seria un solo elemento
@@ -38,12 +33,31 @@ class ListaEnlazada:
 
         self.numero_elementos += 1
 
-    def get_last_node(self):
-        """Método para obtener el ultimo nodo"""
+    def add_between(self, index, data):
+        """Función que agrega un elemento entre nodos"""
+
+        if index > self.numero_elementos: 
+            return "Error: List out of range"
+
+        # Si queremos agregar entre el primer y segundo nodo, osea en el indice 0
+        if index == 0:
+            self.add_at_start(data)
+            return
+        
+        # Iniciamos desde el principio de la lista
         curr = self.head
-        while curr.next:
+
+        # Posiciononarse en el nodo que necesito (indice - 1)
+        for _ in range(index - 1):
             curr = curr.next
-        return curr.data
+        
+        new_node = Node(data=data, next=curr.next)
+        curr.next = new_node
+        self.numero_elementos += 1
+    
+    def is_empty(self):
+        """Método para verificar si la estructura de datos esta vacia"""
+        return self.head is None
 
     def exist(self,data):
         """Método para saber si existe un nodo con cierto valor"""
@@ -63,8 +77,6 @@ class ListaEnlazada:
             if curr.next is None:
                 return False
         return True
-
-
 
     def print_list( self, nombre ):
         """Método para imprimir la lista de nodos"""
@@ -102,49 +114,87 @@ class ListaEnlazada:
         
         return recursive(n, node)
     
-    def add_between(self, index, data):
-        """Función que agrega un elemento entre nodos"""
-
-        if index > self.numero_elementos: 
-            return "Error: List out of range"
-
-        # Si queremos agregar entre el primer y segundo nodo, osea en el indice 0
-        if index == 0:
-            self.add_at_start(data)
-            return
-        
-        # Iniciamos desde el principio de la lista
-        curr = self.head
-
-        # Posiciononarse en el nodo que necesito (indice - 1)
-        for _ in range(index - 1):
-            curr = curr.next
-        
-        new_node = Node(data=data, next=curr.next)
-        curr.next = new_node
-        self.numero_elementos += 1
-    
     def edit(self,index, data):
+        """Edita la lista en un indice dado"""
         if self.is_empty():
             return False
+        
         curr = self.head
-
-        if self.size()-1 < index:
+        if self.size() - 1  <  index:
             print("Error")
             return False
 
         for _ in range(index - 1):
             curr = curr.next
+        
         curr.data = data
         return True
 
     def size(self):
+        """ret"""
         return self.numero_elementos
 
-    # def delete_element(self, index):
-    #     """Función que elimina el elemento en el índice dado de la lista"""
+    def eliminar(self, index):
+        """Elimina un elemento de la lista en un indice dado"""
+        if self.is_empty():
+            return False
+        
+        if index >= self.size():
+            print("Error, list out of range") 
+            return False
 
-    #     if index == 0:
+        if index == 0:
+            second_element = self.head.next
+            self.head = second_element
+            self.numero_elementos -= 1
+            return True
+        
+        if index == self.size() - 1:
+            curr = self.head
+            for _ in range(self.size() - 1):
+                curr = curr.next
+            curr.next  = None
+            
+            self.numero_elementos -= 1
+            return True
+        
+        curr = self.head
+        for i in range(index - 1):
+            curr= curr.next
+        
+        self.numero_elementos -= 1
+        curr.next = curr.next.next
+        return True
 
-    #     return 
+        
+        
+        
 
+
+
+
+
+
+# lista = ListaEnlazada()
+# lista.add_at_end(2)
+# lista.add_at_end(4)
+# lista.add_at_end(7)
+# lista.add_at_end(5)
+# lista.add_at_end(1)
+# lista.add_at_end(7)
+# lista.add_at_end(1)
+# lista.add_at_end(19)
+# lista.add_at_end(1)
+# lista.add_at_end(7)
+# lista.add_at_end(19)
+
+# lista.print_list("A")
+
+# lista.eliminar(2)
+
+# lista.print_list("A")
+# lista.eliminar(0)
+
+# lista.print_list("A")
+# lista.eliminar(lista.size() - 1)
+# lista.print_list("A")
