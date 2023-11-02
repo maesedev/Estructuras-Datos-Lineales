c318631deee7f42d1ed8a9331329e7474a3cec82
santiago Garzon <79063950+maesedev@users.noreply.github.com>
2023-11-02 15:39:37 +0000

    refactor cambiamos el array de arrays de arrays por una clase de python

--- corte3/ordenamiento/randix-sort/ejercicio #2 31-10.py
+++ corte3/ordenamiento/randix-sort/ejercicio #2 31-10.py
@@ -1,66 +1,69 @@
 from random import randint
+from statistics import mean
 
-def randix_sort_modificado(codigos_list, cantidades_list, precios_list):
 
-    # [ [0] , [1] , [2],..., [9] ]
-    # [ [ 1000,  cantidad ,  [precios] ]  ,
-    #                   ,...,
-    #  [ [1090] ,  cantidad ,  [precios] ]]
+class producto():
+    def __init__(self, codigo, cantidad, precios):
+        self.codigo = codigo
+        self.cantidad = cantidad
+        self.precios = precios
 
+
+def randix_sort_modificado(productos):
+    """ ordena los productos  """
     
+    
+    digitos = [ producto(codigo=None, cantidad=0 , precios = [])  for i in range(0,10)]
 
-    digitos = [ [None , 0 , []]  for i in range(0,10)]
-    N_elem =  len(codigos_list)
-    print("\nlista previa: ",codigos_list)
+    N_elem =  len(productos)
+
+    str="["
+    for i in productos:
+        str += f"{i.codigo}, "
+    print(str+"]")
+
     print("\nOrdenando...\n")
 
     for i in range(0, N_elem):
-        index = int(str(codigos_list[i]) [2] )
 
-        digitos[ index ][0] = codigos_list[i]
-        digitos[ index ][1] += cantidades_list[i]
-        digitos[ index ][2].append(precios_list[i])
-        
-    # Vaciar las listas
-    codigos_list = []
-    cantidades_list = []
-    precios_list = []
+        index = int((""+productos[i].codigo)[2])
+
+        digitos[ index ].codigo = productos[i].codigo
+        digitos[ index ].cantidad += productos[i].cantidad
+        digitos[ index ].precios.append(productos[i].precios)
+
+    productos = []
 
     ## volver a llenar las listas
     for item in digitos:
+        
         # En el caso de que no se haya llenado una posicion, no lo agregamos a las listas finales
-        if item[0] is None:
+        if item.codigo is None:
             continue
-        codigos_list.append( item[0] )
-        cantidades_list.append( item[1] )
-        precios_list.append( round(sum(item[2]) / len(item[2])    ,2) )
-    # retornar las listas ordenadas
-    return [codigos_list, cantidades_list, precios_list]
+        productos.append(producto(codigo=None, cantidad=0 , precios = []))
+        productos[-1].codigo =  item.codigo 
+        productos[-1].cantidad =  item.cantidad 
+        
+        productos[-1].precios = round( mean(item.precios) )
+
+    # retornar los productos
+    return productos
 
 
-codigos     = []
-cantidades  = []
-precios     = []
 
-N = randint(6, 15)
+productos  = []
 
-# LLENAR LAS LISTAS CON DATOS ALEATORIOS #
+N = randint(6, 20)
+
+# LLENAR CON DATOS ALEATORIOS #
 for i in range(0,N):
-    codigos.append( f"10{ randint(0,9) }0" )
-    cantidades.append( randint(0,59) )
-    precios.append( randint(1, 30) * 10 )
+    productos.append( producto(codigo=f"10{ randint(0,9) }0", cantidad=randint(0,59) , precios = randint(1, 30) * 10) )
 
+for product in productos:
+    print(f"Codigo: {product.codigo} \tCantidad: {product.cantidad} \tPrecio: {product.precios}")
 
-for i in range(0,N):
-    print(f"Codigo: {codigos[i]} \tCantidad: {cantidades[i]} \tPrecio: {precios[i]}")
+productos = randix_sort_modificado(productos)
 
+for product in productos:
+    print(f"Codigo: {product.codigo} \tCantidad: {product.cantidad} \tPrecio: {product.precios}")
 
-[
-codigos_ordenado,
-cantidades_ordenado,
-precios_ordenado
-]  =                    randix_sort_modificado(codigos,cantidades, precios )
-
-
-for i in range(0,len(codigos_ordenado)):
-    print(f"Codigo: {codigos_ordenado[i]} \tCantidad: {cantidades_ordenado[i]} \tPrecio: {precios_ordenado[i]}")
